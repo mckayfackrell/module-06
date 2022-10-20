531eb06a2027b9c3395ab134b111d983ca1e7f57
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-10-20 13:57:26 -0600

    Added 5 day forecast section with card deck and placeholders. Added a today's date with city name card.  Created header section and moved nav inside.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,31 @@
 https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
+var dayOfWeek1
+var card-temp1
+var card-wind1
+var card-humidity1
+
+var dayOfWeek2
+var card-temp2
+var card-wind2
+var card-humidity2
+
+var dayOfWeek3
+var card-temp3
+var card-wind3
+var card-humidity3
+
+var dayOfWeek4
+var card-temp4
+var card-wind4
+var card-humidity4
+
+var dayOfWeek5
+var card-temp5
+var card-wind5
+var card-humidity5
+
+var dayOfWeek
+var card-temp
+var card-wind
+var card-humidity
