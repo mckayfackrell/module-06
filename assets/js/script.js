41a9c6451bca0f4302fe0858ee5e23022dc3fdee
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-10-22 17:02:06 -0600

    Fixed html layout. Added a notes file. Updated script with minor API information.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,8 @@
-https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
 
-var dayOfWeek1
-var card-temp1
-var card-wind1
-var card-humidity1
+var APIKey = "e33fb596a1cab070a75ee3b5b9aa3a27";
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-var dayOfWeek2
-var card-temp2
-var card-wind2
-var card-humidity2
-
-var dayOfWeek3
-var card-temp3
-var card-wind3
-var card-humidity3
-
-var dayOfWeek4
-var card-temp4
-var card-wind4
-var card-humidity4
-
-var dayOfWeek5
-var card-temp5
-var card-wind5
-var card-humidity5
-
-var dayOfWeek
-var card-temp
-var card-wind
-var card-humidity
+fetch(queryURL)
+console.log(queryURL)
