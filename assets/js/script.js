eadd103330bd625fdff11382530c6ba7d0d1545b
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-10-22 17:03:17 -0600

    commented out url in the meantime in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
+// api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
 
 var APIKey = "e33fb596a1cab070a75ee3b5b9aa3a27";
 var city;
