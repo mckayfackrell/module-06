823d6c88bb2a64dba02d691016cdcc9111001bf0
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-10-22 19:07:46 -0600

    added code from insomnia app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 // api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
 
+// const options = {method: 'GET'};
+
+// fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=e33fb596a1cab070a75ee3b5b9aa3a27', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
 var APIKey = "e33fb596a1cab070a75ee3b5b9aa3a27";
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
