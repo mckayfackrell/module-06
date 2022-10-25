7d225210440c0953e70322821297299b5892e42a
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-10-25 17:08:40 -0600

    added js to get current day and display next to the city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-// api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
+// This gets the current day and displays in the id tag of the main card block which also displays the city name
+let currentDay = document.getElementById("currentDay");
+      let today = new Date();
+      let day = `${(today.getDate()) < 10 ? "0" : ""}${today.getDate()}`;
+      let month = `${(today.getMonth() + 1) < 10 ? "0" : ""}${today.getMonth() + 1}`;
+      let year = today.getFullYear();
 
-// const options = {method: 'GET'};
-
+      currentDay.textContent = `${month} / ${day} / ${year}`;
 
 //This url is only used to get the weather data for the current date and time
-
 let weather = {
   apiKey: "ea18aa3b77d0e7dd90319c2b59567cb8",
   fetchWeather: function(city){
@@ -15,10 +18,16 @@ let weather = {
         + this.apiKey
     )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => this.displayWeather(data));
   },
   displayWeather: function(data) {
-
+    const { name } = data;
+    const { icon } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, temp, humidity, speed);
+    document.querySelector(".city").innerText = name;
+    
   }
 };
 
@@ -26,6 +35,11 @@ let weather = {
 //   .then(response => response.json())
 //   .then(response => console.log(response))
 //   .catch(err => console.error(err));
+
+
+// api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
+
+// const options = {method: 'GET'};
 
 
 
