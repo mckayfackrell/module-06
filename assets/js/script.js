086e54f5eb8e628cf03b2d2762ac3d1bb730fa05
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-10-25 18:19:26 -0600

    added all class tags for main card data except for the icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,16 +22,19 @@ let weather = {
   },
   displayWeather: function(data) {
     const { name } = data;
-    const { icon } = data.weather[0];
+    const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     console.log(name, icon, temp, humidity, speed);
     document.querySelector(".city").innerText = name;
-    
+    // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/10d@2x.png";// "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".temp").innerText = "Temp: " + temp +" °F";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity +"%";
+    document.querySelector(".speed").innerText = "Wind: " + speed + " MPH";
   }
 };
 
-
+weather.fetchWeather("draper")
 //   .then(response => response.json())
 //   .then(response => console.log(response))
 //   .catch(err => console.error(err));
